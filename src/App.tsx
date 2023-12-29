35fec08c443602a3ca1d54132000348dd7142ffb
Tomasz Suski <tomasz.suski@orba.co>
2023-12-29 14:04:40 +0100

    added simple validation to submit and changed layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ function App() {
 
   return (
     <div className="App">
-      <Todos items={todos} />
       <NewTodo onAddTodo={AddTodoHandler} todos={todos} />
+      <Todos items={todos} />
     </div>
   );
 }
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Todo from "../models/todo";
 
-const NewTodo: React.FC<{ onAddTodo: (id: number, text: string) => void, todos: Todo[] }> = (props) => {
-  const textInputRef = React.useRef<HTMLInputElement>(null);
+const NewTodo: React.FC<{
+  onAddTodo: (id: number, text: string) => void;
+  todos: Todo[];
+}> = (props) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
 
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const enteredText = textInputRef.current!.value;
-    const newId = props.todos.length >= 1 ? props.todos[props.todos.length - 1].id + 1 : 1;
-    props.onAddTodo(newId, enteredText);
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+    const newId =
+      props.todos.length >= 1 ? props.todos[props.todos.length - 1].id + 1 : 1;
+    props.onAddTodo(newId, enteredText as string);
+    textInputRef.current!.value = "";
   };
 
   return (
